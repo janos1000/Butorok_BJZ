d2f406aea95861da56d809d1048f1c2ae28be9c8
janos1000 <birinyij@kkszki.hu>
2025-09-24 09:19:45 +0200

    Add project files.

---
+++ Butorok_BJZ/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Butorok_BJZ
+{
+    internal class Program
+    {
+        public class Butorlap
+        {
+            public int hosszusag;
+            public int szelleseg;
+            public string tipus;
+
+        }
+        static void Main(string[] args)
+        {
+        }
+    }
+}
